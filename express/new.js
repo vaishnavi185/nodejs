81b5c54111fd631414c5e6dde3aad7fdd6d35bc1
vaishnavi185 <vishu032004@gmail.com>
2024-05-08 20:38:50 +0530

    new

--- express/new.js
+++ express/new.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// var express = require('express')
+// var app = express();
+// app.get('/',function(req,res){
+//   res.send("hello world")
+// });
+// app.listen(3000);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// var express = require('express')
+// var app = express();
+// app.get('/home',function(req,res){
+//   res.send("this is home")
+// });
+// app.listen(4000)
+// var express = require('express');
+// var app = express();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.get('/:id', function(req, res) {
+//   res.send('the id: ' + req.params.id);
+// });
+
+// app.listen(5000);
