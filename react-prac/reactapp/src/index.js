d75f1d36076812e2e0c8a56863240bdaab62bf52
vaishnavi <vishu032004@gmail.com>
2025-05-29 11:06:40 +0530

    tp

--- react-prac/reactapp/src/index.js
+++ react-prac/reactapp/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+const tapp="hello"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {tapp}
+    {/* <App /> */}
   </React.StrictMode>
 );
 
