706c87d9a0ca4eb9b42a1493bb058b77e35f2a2c
vaishnavi <vishu032004@gmail.com>
2025-05-30 11:26:00 +0530

    nested list

--- react-prac/reactapp/src/Des.jsx
+++ react-prac/reactapp/src/Des.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 
 export default function Des() {
     const phoneDiary = [
-  { name: 'Vaishnavi', phone: '123-456-7890' },
-  { name: 'Tan', phone: '987-654-3210' },
+  { name: 'Vaishnavi', phone: '123-456-7890',
+  
+ address: [
+  {streeet:"galino9",city:"chennai"},
+  {streeet:"galino0",city:"chennai"}
+  ]  }
 ];
 const phnlist=phoneDiary.map((item,index)=>{
     return(
         <div key={(index)}>
 name:{item.name},phone:{item.phone}
+{item.address.map((addr,addin)=>{
+  return(
+    <div key={addin}>
+      Street: {addr.streeet}, City: {addr.city}
+    </div>
+  )
+})}
         </div>
     )
 })
