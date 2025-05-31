b9dbba4ed9fa5208eabc7528ef356335f8affb90
vaishnavi <vishu032004@gmail.com>
2025-05-31 11:44:23 +0530

    class components

--- react-prac/reactapp/src/index.js
+++ react-prac/reactapp/src/index.js
@@ -1,35 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Des from './Des';
-import reportWebVitals from './reportWebVitals';
-
-function Car(props){
-  return (
-    <h1>hi i am {props.color}</h1>
-  )
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964
+    };
+  }
+  changeColor = () => {
+    this.setState({color: "blue"});
+  }
+  render() {
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          It is a {this.state.color}
+          {this.state.model}
+          from {this.state.year}.
+        </p>
+        <button
+          type="button"
+          onClick={this.changeColor}
+        >Change color</button>
+      </div>
+    );
+  }
 }
-function Grage(){
-  return(
-    <>
-    <h1>who is there</h1>
-    <Car color="red"></Car>
-    </>
-  )
-}
-const tapp="hello"
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {/* <App></App> */}
-    <Des></Des>
-    {/* <Car color="red"></Car>  */}
-    <Grage></Grage>
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<Car />);
