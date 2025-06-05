77779116be8dde1c70691dbfbb27ae8a721b0950
vaishnavi <vishu032004@gmail.com>
2025-06-05 20:03:32 +0530

    constructore

--- Pro.java
+++ Pro.java
@@ -4,11 +4,19 @@ class Pro{
     void GetN(){
         System.out.println("n="+n);
     }
+void Pro(){
+    System.out.println("i am no args constructor");
+}
 
+void Pro(int a){
+    System.out.println("Prameter"+a);
+}
     public static void main(String[] args) {
         Pro p = new Pro();
         p.GetN();
         System.out.print(+m);
+        p.Pro();
+        p.Pro(900);
 
         int a= 100;
         System.out.println(+a);
