c2e8985e6f1f265c390e034773eda944c3d11035
vaishnavi <vishu032004@gmail.com>
2025-06-07 21:04:05 +0530

    strings

---
+++ S.java
@@ -0,0 +1,8 @@
+public class S {
+    public static void main(String[] args) {
+      
+        String str=new String ("hello");
+  String s="hello";
+        System.out.println(s==str); // false, because s is a string literal and str is an object
+    }
+}
