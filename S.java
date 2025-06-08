0887837dd18d6e9b59eade3080686bd68671b550
vaishnavi <vishu032004@gmail.com>
2025-06-08 17:39:45 +0530

    empty

--- S.java
+++ S.java
@@ -1,7 +1,7 @@
 public class S {
     public static void main(String[] args) {
       
-        String str=new String ("hello");
+        String str=new String ("hllo");
   String s="hello";
         System.out.println(s==str); // false, because s is a string literal and str is an object
     }
