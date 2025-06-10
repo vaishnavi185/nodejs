59a007e2cb6459822e63fc6aed745a1f860153c6
vaishnavi <vishu032004@gmail.com>
2025-06-10 10:02:31 +0530

    strings in java

--- S.java
+++ S.java
@@ -1,8 +1,21 @@
-public class S {
-    public static void main(String[] args) {
+// public class S {
+//     public static void main(String[] args) {
       
-        String str=new String ("hllo");
-  String s="hello";
-        System.out.println(s==str); // false, because s is a string literal and str is an object
+//         String str=new String ("hllo");
+//   String s="hello";
+//         System.out.println(s==str); // false, because s is a string literal and str is an object
+//     }
+// }
+
+
+class S{
+    public static void main(String[] args) {
+        StringBuffer sb= new StringBuffer();
+        sb.append("hello");
+        sb.append("world");
+        sb.insert(5, " ");
+
+        String str = sb.toString();
+        System.out.println(str);
     }
 }
