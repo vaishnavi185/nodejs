c54d78ba2e7fcab0bffa5492e4194f4d7ee3e5a7
vaishnavi <vishu032004@gmail.com>
2025-06-10 10:24:47 +0530

    capitalized

--- S.java
+++ S.java
@@ -8,20 +8,36 @@
 // }
 
 
-class S{
-    public static void main(String[] args) {
-        StringBuffer sb= new StringBuffer();
-        sb.append("hello");
-        sb.append("world");
-        sb.insert(5, " ");
+// class S{
+//     public static void main(String[] args) {
+//         StringBuffer sb= new StringBuffer();
+//         sb.append("hello");
+//         sb.append("world");
+//         sb.insert(5, " ");
 
-        sb.reverse() ;
+//         sb.reverse() ;
     
 
-        String str = sb.toString();
-        System.out.println(str);
-        System.out.println(sb.capacity());
-        System.out.print(sb.charAt(8));
+//         String str = sb.toString();
+//         System.out.println(str);
+//         System.out.println(sb.capacity());
+//         System.out.print(sb.charAt(8));
 
+//     }
+// }
+
+class S{
+     public static void main(String[] args) {
+        String str = "java is*fun";
+       String[] words = str.split("\\*");
+
+        StringBuilder capitalized = new StringBuilder();
+
+        for(String word : words) {
+            capitalized.append(Character.toUpperCase(word.charAt(0)))
+                       .append(word.substring(1)).append(" ");
+        }
+
+        System.out.println(capitalized.toString().trim());
     }
 }
